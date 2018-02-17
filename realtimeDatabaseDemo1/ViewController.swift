cc9f1b909fe9031f941f9e75d63ec41379eaecef
shenfive @ Mac <shenfive@gmail.com>
2018-02-17 23:28:22 +0800

    Initial Commit

---
+++ realtimeDatabaseDemo1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  realtimeDatabaseDemo1
+//
+//  Created by 申潤五 on 2018/2/17.
+//  Copyright © 2018年 申潤五. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
